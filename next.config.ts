d840c53870ee3fb0ffd31d6d51248542ec252d68
alphamouhamadou <121497396+alphamouhamadou@users.noreply.github.com>
2026-03-17 05:53:35 +0000

    Ignore TypeScript build errors

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true
   },
   async rewrites() {
     return [
-      // Rediriger toutes les routes vers la page d'accueil pour le SPA
       { source: '/connexion', destination: '/' },
       { source: '/membre', destination: '/' },
       { source: '/admin', destination: '/' },
